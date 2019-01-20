efc2a358654e4d071628dc41e25258dd75fe82b8
mynktwri <mayank@rockinfamily.com>
2019-01-20 00:33:54 -0800

    remove uuid

--- nodejs-app/app.js
+++ nodejs-app/app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send(`Hello, ${uuid()}!`);
+  res.status(200).send(`Hello, world!`);
 });
 
 // Start the server
